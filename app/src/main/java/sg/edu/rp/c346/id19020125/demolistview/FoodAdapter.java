d8911d23e35b8f52688fe298d49b9ad0ce1880d7
19020125 <19020125@myrp.edu.sg>
2021-04-20 12:18:21 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19020125/demolistview/FoodAdapter.java
@@ -0,0 +1,45 @@
+package sg.edu.rp.c346.id19020125.demolistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import sg.edu.rp.c346.id19020125.demolistview.Food;
+import sg.edu.rp.c346.id19020125.demolistview.R;
+
+public class FoodAdapter extends ArrayAdapter<Food> {
+    private ArrayList<Food> food;
+    private Context context;
+    private TextView tvFoodName;
+    private ImageView ivStar;
+
+    public FoodAdapter(Context context, int resource, ArrayList<Food> objects) {
+        super(context, resource, objects);
+        food = objects;
+        this.context = context;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.row, parent, false);
+        tvFoodName = (TextView) rowView.findViewById(R.id.tvFoodName);
+        ivStar = (ImageView) rowView.findViewById(R.id.ivStar);
+
+        Food currentFood = food.get(position);
+        tvFoodName.setText(currentFood.getName());
+        if(currentFood.isStar()) {
+            ivStar.setImageResource(R.drawable.star);
+        }
+        else {
+            ivStar.setImageResource(R.drawable.nostar);
+        }
+        return rowView;
+    }
+
+}
